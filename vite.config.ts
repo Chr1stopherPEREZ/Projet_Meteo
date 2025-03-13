549fda2a5d4845ee61c102a91f3e143f684f01cc
Christopher PEREZ <christopher11perez@hotmail.com>
2025-03-13 12:14:53 +0100

    Correction config Vite.v3

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,4 @@ export default defineConfig({
     exclude: ["lucide-react"],
   },
   base: "/Projet_Meteo/",
-  build: {
-    assetsDir: "", // Empêche Vite de renommer les fichiers avec un hash
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
 });
