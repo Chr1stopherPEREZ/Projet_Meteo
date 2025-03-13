5e4d5712afe6dcdb91a3a46d71236bc928c7e086
Christopher PEREZ <christopher11perez@hotmail.com>
2025-03-13 12:03:30 +0100

    Correction du chemin du script et config Vite.v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
   base: "/Projet_Meteo/",
+  build: {
+    assetsDir: "", // Empêche Vite de renommer les fichiers avec un hash
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
 });
